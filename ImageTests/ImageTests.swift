bbd33fdfd813a3b24f6eccbe04b2523f6db32ad6
Aries672 <Aries@ip-118-202-42-198.neu.edu.cn>
2015-12-01 14:02:08 +0800

    Initial Commit

---
+++ ImageTests/ImageTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageTests.swift
+//  ImageTests
+//
+//  Created by Aries on 12/1/15.
+//  Copyright © 2015 star. All rights reserved.
+//
+
+import XCTest
+@testable import Image
+
+class ImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
